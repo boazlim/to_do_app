13ef41583b2070375f69d8aba249e1f3fd30cb39
Andy Lam <andy@Andys-Air-4.lan>
2023-11-22 23:57:45 -0500

    a

--- lib/calendar_screen.dart
+++ lib/calendar_screen.dart
@@ -7,14 +7,115 @@ class CalendarScreen extends StatefulWidget {
   @override
   _CalendarScreenState createState() => _CalendarScreenState();
 }
+
 class _CalendarScreenState extends State<CalendarScreen> {
+  List<Appointment> appointments;
+
+  _CalendarScreenState() : appointments = getAppointments();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SfCalendar(
         view: CalendarView.week,
-        firstDayOfWeek: 6,
-        dataSource: MeetingDataSource(getAppointments()),
+        firstDayOfWeek: 7,
+        dataSource: MeetingDataSource(appointments),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _onAddAppointmentPressed,
+        tooltip: 'Add Appointment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+
+  void _onAddAppointmentPressed() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        // Use a StatefulWidget if you need more than one input field
+        // to manage the state of the inputs.
+        return AddAppointmentDialog(onAdd: (Appointment newAppointment) {
+          setState(() {
+            appointments.add(newAppointment);
+          });
+        });
+      },
+    );
+  }
+}
+
+class AddAppointmentDialog extends StatefulWidget {
+  final Function(Appointment) onAdd;
+
+  AddAppointmentDialog({Key? key, required this.onAdd}) : super(key: key);
+
+  @override
+  _AddAppointmentDialogState createState() => _AddAppointmentDialogState();
+}
+
+class _AddAppointmentDialogState extends State<AddAppointmentDialog> {
+  final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _startTimeController = TextEditingController();
+  final TextEditingController _endTimeController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextField(
+              controller: _subjectController,
+              decoration: const InputDecoration(hintText: 'Subject'),
+            ),
+            TextField(
+              controller: _startTimeController,
+              decoration: const InputDecoration(hintText: 'Start Time (HH:MM)'),
+              keyboardType: TextInputType.datetime,
+            ),
+            TextField(
+              controller: _endTimeController,
+              decoration: const InputDecoration(hintText: 'End Time (HH:MM)'),
+              keyboardType: TextInputType.datetime,
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                // Here you would add validation and logic to ensure the times are correct
+                // For simplicity, we're assuming the user inputs valid data
+                final DateTime now = DateTime.now();
+                final DateTime startTime = DateTime(
+                  now.year,
+                  now.month,
+                  now.day,
+                  int.parse(_startTimeController.text.split(':')[0]),
+                  int.parse(_startTimeController.text.split(':')[1]),
+                );
+                final DateTime endTime = DateTime(
+                  now.year,
+                  now.month,
+                  now.day,
+                  int.parse(_endTimeController.text.split(':')[0]),
+                  int.parse(_endTimeController.text.split(':')[1]),
+                );
+                
+                final Appointment newAppointment = Appointment(
+                  startTime: startTime,
+                  endTime: endTime,
+                  subject: _subjectController.text,
+                  color: Colors.blue,
+                );
+
+                widget.onAdd(newAppointment);
+                Navigator.pop(context);
+              },
+              child: const Text('Add Appointment'),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -36,7 +137,7 @@ List<Appointment> getAppointments() {
   return meetings;
 }
 
-class MeetingDataSource extends CalendarDataSource{
+class MeetingDataSource extends CalendarDataSource {
   MeetingDataSource(List<Appointment> source) {
     appointments = source;
   }
